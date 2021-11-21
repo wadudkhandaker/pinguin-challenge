0b4053a9fae10a448b24d824aa61eb2ebae6deca
wadudKhandaker <wadud.khandaker@selise.ch>
2021-11-21 18:07:38 +0700

    refactor: package name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-scafold'),
+      dir: require('path').join(__dirname, './coverage/angular-challenge'),
       subdir: '.',
       reporters: [
         { type: 'html' },
